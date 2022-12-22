286af0c427bff5a46560c31e6c566707ac3db94d
elios11 <eliosferre3@hotmail.com>
2022-12-22 04:44:16 -0300

    Initialize repository

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css"
+
+export default function App() {
+    return (
+        <div>
+            <h1>
+                Elías Ferreira - Portfolio
+            </h1>
+        </div>
+    )
+}
