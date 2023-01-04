f39d51592b0e5b2d44ca1a64157be9c0b47a2b12
elios11 <eliosferre3@hotmail.com>
2023-01-03 21:21:49 -0300

    Display home-page with Footer and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import "./App.css"
+import Navbar from "./components/Navbar/Navbar"
+import Footer from "./components/Footer/Footer"
 
 export default function App() {
     return (
-        <div>
-            <h1>
-                Elías Ferreira - Portfolio
-            </h1>
-        </div>
+        <>
+            <Navbar />
+            <main>
+                <h1 className="title">
+                    ELÍAS FERREIRA
+                </h1>
+                <p className="main-text">
+                    I am a Uruguayan front end developer, I currently develop in React and am learning Scrum Master and Angular.
+                </p>
+                <p className="main-text">
+                    I aim to develop clean code and good practices, eager to make the improvements I know can 
+                    make to my code as soon as I finish a big portion of it and reworking my whole work when necessary.
+                </p>
+            </main>
+            <Footer />
+        </>
     )
 }
