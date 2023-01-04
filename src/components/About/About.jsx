379da87cf51dd54e532d56219abbbdbf3c5f4226
elios11 <eliosferre3@hotmail.com>
2023-01-04 16:36:13 -0300

    Add About page component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,23 @@
+import "./About.css"
+
+export default function About() {
+    return (
+        <div className="about">
+            <p>
+                I am 21 years and started getting interested in programming when I was 14.
+                Then I decided I wanted to work developing software apps. When I finally got my hands
+                onto coding, I learned about web development and loved it.
+            </p>
+            <p>
+                I started with The Odin Project to learn the basics and then jumped into CS50x Harvard’s
+                course. I tend to vary a lot my learning sources and am always trying to update my knowledge
+                in any topic I learn to improve my code and practices.
+            </p>
+            <p>
+                I finally took a programming course which included transversal skills and fully dove into 
+                web development where I learned client-server model and the use of technologies like 
+                Bootstrap, Sass, Node, Express and asynchrony in JavaScript, as well as the use of the fetch and localStorage APIs.
+            </p>
+        </div>
+    )
+}
