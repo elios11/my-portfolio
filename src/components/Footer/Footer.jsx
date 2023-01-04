2b2bd3d3337d7bb05a33e06b6baa213ecf95a182
elios11 <eliosferre3@hotmail.com>
2023-01-03 21:20:23 -0300

    Add Footer and Navbar components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css"
+
+export default function Footer() {
+    return (
+        <footer>
+            <a href="https://www.linkedin.com/in/eliosferre/" target="_blank">
+                <img src="/linkedin-logo.png" alt="LinkedIn logo" />
+            </a>
+            <a href="https://github.com/elios11" target="_blank">
+                <img src="/github-logo.png" alt="GitHub logo" />
+            </a>
+        </footer>
+    )
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import "./Navbar.css"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <a href="#" className="active">Home</a>
+            <a href="#about">About</a>
+            <a href="#projects">Projects</a>
+            <a href="#contact">Contact</a>
+        </nav>
+    )
+}
