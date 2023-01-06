d53c23583f30c536edd99fec5162f514645a9964
elios11 <eliosferre3@hotmail.com>
2023-01-05 22:55:54 -0300

    Add contact route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar"
 import Footer from "./components/Footer/Footer"
 import Main from "./components/Main/Main"
 import About from "./components/About/About"
+import ContactForm from "./components/ContactForm/ContactForm"
 import { Route, Routes } from "react-router-dom"
 
 export default function App() {
@@ -12,6 +13,8 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<Main />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<></>} />
+                <Route path="/contact" element={<ContactForm />} />
             </Routes>
             <Footer />
         </>
