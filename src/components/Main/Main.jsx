7f4f4b224852af97640b0a88f052b143c5177050
elios11 <eliosferre3@hotmail.com>
2023-04-10 14:22:58 -0300

    Add framer motion animations to Main page

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,18 +1,28 @@
 import "./Main.css"
+import { motion } from "framer-motion"
+import { titleVariants, paragraphVariants, containerVariants } from "../../utils/transitionVariants"
 
 export default function Main() {
     return (
-        <main>
-            <h1 className="title">
+        <motion.main
+            variants={ containerVariants }
+            initial="start"
+            animate="end"
+            transition={{ when: "beforeChildren", staggerChildren: 1 }}
+            exit="toLeft"
+        >
+            <motion.h1 variants={ titleVariants } className="title">
                 ELÍAS FERREIRA
-            </h1>
-            <p className="main-text">
-                I am a Uruguayan front end developer, I currently develop in React and am learning Scrum Master and Angular.
-            </p>
-            <p className="main-text">
+            </motion.h1>
+            <motion.p variants={ paragraphVariants } className="main-text">
+                I am a Uruguayan front end developer, I currently develop in React and have the fundamentals
+                of working with agile methodologies.
+                I'm mostly self-taught and I always love learning new technologies that could fit any of my projects.
+            </motion.p>
+            <motion.p variants={ paragraphVariants } className="main-text">
                 I aim to develop clean code and good practices, eager to make the improvements I know can
                 make to my code as soon as I finish a big portion of it and reworking my whole work when necessary.
-            </p>
-        </main>
+            </motion.p>
+        </motion.main>
     )
 }
