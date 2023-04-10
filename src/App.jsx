0d198c5c0554ba66089955c087e6ae433cf1edaf
elios11 <eliosferre3@hotmail.com>
2023-04-10 14:20:31 -0300

    Add background video and framer motion animation components

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,26 @@ import Footer from "./components/Footer/Footer"
 import Main from "./components/Main/Main"
 import About from "./components/About/About"
 import ContactForm from "./components/ContactForm/ContactForm"
-import { Route, Routes } from "react-router-dom"
+import Projects from "./components/Projects/Projects"
+import { Route, Routes, useLocation } from "react-router-dom"
+import { AnimatePresence } from "framer-motion"
 
 export default function App() {
+    let location = useLocation()
     return (
         <>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/projects" element={<></>} />
-                <Route path="/contact" element={<ContactForm />} />
-            </Routes>
+            <Navbar/>
+            <video autoPlay muted loop playsInline>
+                <source src="/background-video.mp4" type="video/mp4"/>
+            </video>
+            <AnimatePresence mode="wait">
+                <Routes location={ location } key={ location.key }>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/contact" element={<ContactForm />} />
+                </Routes>
+            </AnimatePresence>
             <Footer />
         </>
     )
