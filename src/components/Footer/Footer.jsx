0044d7d73f88f608917d3095799614270058b413
elios11 <eliosferre3@hotmail.com>
2023-04-11 15:08:30 -0300

    Add mounting animation

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,19 @@
+import { motion } from "framer-motion"
 import "./Footer.css"
 
 export default function Footer() {
     return (
-        <footer>
+        <motion.footer
+            initial={{ opacity: 0, x: "100vw" }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.4 }}
+        >
             <a href="https://www.linkedin.com/in/eliosferre/" target="_blank">
                 <img src="/linkedin-logo.png" alt="LinkedIn logo" />
             </a>
             <a href="https://github.com/elios11" target="_blank">
                 <img src="/github-logo.png" alt="GitHub logo" />
             </a>
-        </footer>
+        </motion.footer>
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,18 @@
 import { NavLink } from "react-router-dom"
 import "./Navbar.css"
+import { motion } from "framer-motion"
 
 export default function Navbar() {
     return (
-        <nav>
+        <motion.nav
+            initial={{ opacity: 0, x: "-100vh" }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.4 }}
+        >
             <NavLink to="/">Home</NavLink>
             <NavLink to="/about">About</NavLink>
             <NavLink to="/projects">Projects</NavLink>
             <NavLink to="/contact">Contact</NavLink>
-        </nav>
+        </motion.nav>
     )
 }
