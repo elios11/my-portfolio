2a5a22aac08204c101c5205b57a66727eb98133a
elios11 <eliosferre3@hotmail.com>
2023-04-12 19:27:06 -0300

    Add transitions/animations for footer items

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,19 @@
 import { motion } from "framer-motion"
 import "./Footer.css"
+import { buttonVariants } from "../../utils/transitionVariants"
 
 export default function Footer() {
     return (
         <motion.footer
             initial={{ opacity: 0, x: "100vw" }}
             animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.4 }}
+            transition={{ duration: 0.6, type: "spring" }}
         >
             <a href="https://www.linkedin.com/in/eliosferre/" target="_blank">
-                <img src="/linkedin-logo.png" alt="LinkedIn logo" />
+                <motion.img variants={buttonVariants} whileHover="hover" src="/linkedin-logo.png" alt="LinkedIn logo" />
             </a>
             <a href="https://github.com/elios11" target="_blank">
-                <img src="/github-logo.png" alt="GitHub logo" />
+                <motion.img variants={buttonVariants} whileHover="hover" src="/github-logo.png" alt="GitHub logo" />
             </a>
         </motion.footer>
     )
