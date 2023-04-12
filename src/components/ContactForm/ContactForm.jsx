696e662efe2c6aa1c91245c31ad013f51de205b0
elios11 <eliosferre3@hotmail.com>
2023-04-12 19:26:29 -0300

    Add funcionality to Contact Form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,21 +1,46 @@
 import { useState } from "react"
 import "./ContactForm.css"
+import { motion } from "framer-motion"
+import { containerVariants } from "../../utils/transitionVariants"
+import { useLocation } from "react-router-dom"
 
 export default function ContactForm() {
-    const [formData, setFormData] = useState({})
+    useLocation()
+    let [email, setEmail] = useState("")
+    let exitAnimation = window.location.pathname == "/contact" ? "toSamePage" : "toRight"
+
+    function updateEmail(event) {
+        setEmail(event.target.value)
+    }
 
     return (
-        <div className="form-container">
+        <motion.div 
+            className="form-container"
+            variants={ containerVariants }
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ when: "beforeChildren", staggerChildren: 0.5, type: "spring", stiffness: 80 }}
+            exit={ exitAnimation }
+        >
             <h1 className="form-title">Let's work together!</h1>
-            <form>
+            <form action="https://api.web3forms.com/submit" method="POST">
+                <input type="hidden" name="access_key" value="1b123a8b-32f6-45f7-9f57-22fd47dea4f2" />
+                <input type="hidden" name="from_name" value="Portfolio Contact Form" />
+                <input type="hidden" name="subject" value={ email } />
+                <input type="checkbox" name="botcheck" className="hidden" style={{ display: "none" }}></input>
                 <label className="nameLabel" htmlFor="name">Name</label>
-                <input className="nameContainer" type="name" id="name" />
+                <input className="inputName" type="text" name="Name" id="name" required />
                 <label className="emailLabel" htmlFor="email">Email</label>
-                <input className="emailContainer"  type="email" id="email" />
+                <input className="inputEmail" type="email" name="Email" id="email" onChange={ updateEmail } required />
                 <label className="msgLabel" htmlFor="message">Message</label>
-                <textarea className="msgContainer" name="message" id="message"></textarea>
-                <button className="send-button" type="button">SEND</button>
+                <textarea className="inputMsg" name="Message" id="message" required></textarea>
+                <motion.button
+                    className="send-button" type="submit"
+                    whileHover={{ backgroundColor: "white", color: "black" }}
+                >
+                    SEND
+                </motion.button>
             </form>
-        </div>
+        </motion.div>
     )
 }
